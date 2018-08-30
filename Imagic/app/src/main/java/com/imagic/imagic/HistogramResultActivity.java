1ba9e223301e42c331e70cac01c01f9435e037a1
Roland Hartanto <rolandhartanto@gmail.com>
2018-08-30 19:17:57 +0700

    plot array to histogram

--- Imagic/app/src/main/java/com/imagic/imagic/HistogramResultActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/HistogramResultActivity.java
@@ -8,10 +8,16 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistogramResultActivity extends AppCompatActivity {
     private static final int COLOR_VALUE_BOUNDARY = 256;
@@ -26,15 +32,22 @@ public class HistogramResultActivity extends AppCompatActivity {
     private int[] blueValuesCount;
     private int[] grayscaleValuesCount;
 
-    private BarChart redChart;
-    private BarChart greenChart;
-    private BarChart blueChart;
-    private BarChart grayscaleChart;
+    private ProgressBar loadingBar;
+    private BarChart redBarChart;
+    private BarChart greenBarChart;
+    private BarChart blueBarChart;
+    private BarChart grayscaleBarChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histogram_result);
+
+        loadingBar = (ProgressBar) findViewById(R.id.loading_bar);
+        redBarChart = (BarChart) findViewById(R.id.red_bar_chart);
+        greenBarChart = (BarChart) findViewById(R.id.green_bar_chart);
+        blueBarChart = (BarChart) findViewById(R.id.blue_bar_chart);
+        grayscaleBarChart = (BarChart) findViewById(R.id.grayscale_bar_chart);
 
         initializeColorValuesCount();
         Intent intent = getIntent();
@@ -44,11 +57,11 @@ public class HistogramResultActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         imageHeight = imageBitmap.getHeight();
         imageWidth = imageBitmap.getWidth();
+
         countColorValues();
-        Log.v("COBA", "pixel(0,0)=" + redValuesCount[0]);
+        plotArrayToHistogram();
     }
 
     private void initializeColorValuesCount() {
@@ -83,6 +96,41 @@ public class HistogramResultActivity extends AppCompatActivity {
     }
 
     private void plotArrayToHistogram() {
+        List<BarEntry> redBarEntries = new ArrayList<>();
+        List<BarEntry> greenBarEntries = new ArrayList<>();
+        List<BarEntry> blueBarEntries = new ArrayList<>();
+        List<BarEntry> grayscaleBarEntries = new ArrayList<>();
 
+        for(int it = 0; it < COLOR_VALUE_BOUNDARY; it++) {
+            redBarEntries.add(new BarEntry(it, redValuesCount[it]));
+            greenBarEntries.add(new BarEntry(it, greenValuesCount[it]));
+            blueBarEntries.add(new BarEntry(it, blueValuesCount[it]));
+            grayscaleBarEntries.add(new BarEntry(it, grayscaleValuesCount[it]));
+        }
+
+        BarDataSet redBarDataSet = new BarDataSet(redBarEntries, "Red");
+        BarDataSet greenBarDataSet = new BarDataSet(greenBarEntries, "Green");
+        BarDataSet blueBarDataSet = new BarDataSet(blueBarEntries, "Blue");
+        BarDataSet grayscaleBarDataSet = new BarDataSet(grayscaleBarEntries, "Grayscale");
+
+        redBarDataSet.setColor(Color.RED);
+        greenBarDataSet.setColor(Color.GREEN);
+        blueBarDataSet.setColor(Color.BLUE);
+        grayscaleBarDataSet.setColor(Color.LTGRAY);
+
+        BarData redBarData = new BarData(redBarDataSet);
+        BarData greenBarData = new BarData(greenBarDataSet);
+        BarData blueBarData = new BarData(blueBarDataSet);
+        BarData grayscaleBarData = new BarData(grayscaleBarDataSet);
+
+        redBarChart.setData(redBarData);
+        greenBarChart.setData(greenBarData);
+        blueBarChart.setData(blueBarData);
+        grayscaleBarChart.setData(grayscaleBarData);
+
+        redBarChart.invalidate();
+        greenBarChart.invalidate();
+        blueBarChart.invalidate();
+        grayscaleBarChart.invalidate();
     }
 }
