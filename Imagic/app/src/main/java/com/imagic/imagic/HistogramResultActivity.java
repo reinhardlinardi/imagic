3b03da7d1124ca12725792a767e98e73592fdd68
Roland Hartanto <rolandhartanto@gmail.com>
2018-08-30 18:28:55 +0700

    count color elements

--- Imagic/app/src/main/java/com/imagic/imagic/HistogramResultActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/HistogramResultActivity.java
@@ -9,16 +9,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.github.mikephil.charting.charts.BarChart;
+
 import java.io.IOException;
 
 public class HistogramResultActivity extends AppCompatActivity {
+    private static final int COLOR_VALUE_BOUNDARY = 256;
+
     private Bitmap imageBitmap;
     private Uri imageUri;
+    private int imageWidth;
+    private int imageHeight;
+
+    private int[] redValuesCount;
+    private int[] greenValuesCount;
+    private int[] blueValuesCount;
+    private int[] grayscaleValuesCount;
+
+    private BarChart redChart;
+    private BarChart greenChart;
+    private BarChart blueChart;
+    private BarChart grayscaleChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_histogram_result);
 
+        initializeColorValuesCount();
         Intent intent = getIntent();
         imageUri = (Uri) intent.getParcelableExtra("imageUri");
         try {
@@ -26,6 +44,41 @@ public class HistogramResultActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.v("COBA", "pixel(0,0)=" + Color.red(imageBitmap.getPixel(0,0)));
+
+        imageHeight = imageBitmap.getHeight();
+        imageWidth = imageBitmap.getWidth();
+        countColorValues();
+//        Log.v("COBA", "pixel(0,0)=" + Color.red(imageBitmap.getPixel(0,0)));
+    }
+
+    private void initializeColorValuesCount() {
+        redValuesCount = new int[COLOR_VALUE_BOUNDARY];
+        greenValuesCount = new int[COLOR_VALUE_BOUNDARY];
+        blueValuesCount = new int[COLOR_VALUE_BOUNDARY];
+        grayscaleValuesCount = new int[COLOR_VALUE_BOUNDARY];
+
+        for(int it = 0; it < COLOR_VALUE_BOUNDARY; it++) {
+            redValuesCount[it] = 0;
+            greenValuesCount[it] = 0;
+            blueValuesCount[it] = 0;
+            grayscaleValuesCount[it] = 0;
+        }
+    }
+
+    private void countColorValues() {
+        for(int row = 0; row < imageHeight; row++) {
+            for (int col = 0; col < imageWidth; col++) {
+                int pixel = imageBitmap.getPixel(row, col);
+                int red = Color.red(pixel);
+                int blue = Color.blue(pixel);
+                int green = Color.green(pixel);
+                int grayscale = ((red + green + blue) / 3) % 256;
+
+                redValuesCount[red]++;
+                greenValuesCount[green]++;
+                blueValuesCount[blue]++;
+                grayscaleValuesCount[grayscale]++;
+            }
+        }
     }
 }
