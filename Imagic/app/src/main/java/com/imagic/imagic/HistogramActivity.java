3a269e3bda345c31c4d281b652acd32a6d53f747
Roland Hartanto <rolandhartanto@gmail.com>
2018-08-30 10:56:22 +0700

    add histogram activity

---
+++ Imagic/app/src/main/java/com/imagic/imagic/HistogramActivity.java
@@ -0,0 +1,13 @@
+package com.imagic.imagic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistogramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_histogram);
+    }
+}
--- Imagic/app/src/main/java/com/imagic/imagic/MainMenuAdapter.java
+++ Imagic/app/src/main/java/com/imagic/imagic/MainMenuAdapter.java
@@ -1,10 +1,12 @@
 package com.imagic.imagic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -23,13 +25,23 @@ public class MainMenuAdapter extends ArrayAdapter {
         LayoutInflater inflater = context.getLayoutInflater();
         View optionView = inflater.inflate(R.layout.listview_mainmenu_option, null, true);
 
-        TextView optionTitleTextView = (TextView) optionView.findViewById(R.id.mainMenuOptionTitleTextView);
+        LinearLayout optionButton = (LinearLayout) optionView.findViewById(R.id.menu);
+        final TextView optionTitleTextView = (TextView) optionView.findViewById(R.id.mainMenuOptionTitleTextView);
         TextView optionDescriptionTextView = (TextView) optionView.findViewById(R.id.mainMenuOptionDescriptionTextView);
 
         MainMenuOption option = (MainMenuOption) getItem(position);
         optionTitleTextView.setText(option.title);
         optionDescriptionTextView.setText(option.description);
 
+        optionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (optionTitleTextView.getText().equals("Histogram")) {
+                    Intent intent = new Intent(context, HistogramActivity.class);
+                    context.startActivity(intent);
+                }
+            }
+        });
         return optionView;
     }
 }
