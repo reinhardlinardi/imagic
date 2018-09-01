68b66472b88e8177a79d80217b6c930092723432
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-09-01 10:41:32 +0700

    Delete all, new main activity

--- Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
@@ -9,18 +9,9 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Main menu options
-    private static final ArrayList<MainMenuOption> options = new ArrayList<>(Arrays.asList(
-        new MainMenuOption("Histogram", "Show RGB and Grayscale histogram from an image")
-    ));
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MainMenuAdapter adapter = new MainMenuAdapter(this, MainActivity.options);
-        ListView mainMenu = findViewById(R.id.mainMenuListView);
-        mainMenu.setAdapter(adapter);
     }
 }
