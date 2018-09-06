a482d689ca666c7bd27141b1ecd169fb6ce4b693
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-09-06 23:41:48 +0700

    Add seekbar event listener, wrap in Slider class

--- Imagic/app/src/main/java/com/imagic/imagic/ContrastEnhancementActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/ContrastEnhancementActivity.java
@@ -2,6 +2,7 @@ package com.imagic.imagic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class ContrastEnhancementActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ContrastEnhancementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contrast_enhancement);
+
+        Slider redSlider = new Slider(this, R.id.redConstantEqualizationSeekBar, R.id.redConstantEqualizationTextView);
+        Slider greenSlider = new Slider(this, R.id.greenConstantEqualizationSeekBar, R.id.greenConstantEqualizationTextView);
+        Slider blueSlider = new Slider(this, R.id.blueConstantEqualizationSeekBar, R.id.blueConstantEqualizationTextView);
     }
 }
