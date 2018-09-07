9b22808ef68d35a9a44b36773d18fc2b7d888dda
Roland Hartanto <rolandhartanto@gmail.com>
2018-09-08 01:44:31 +0700

    refactor (rename & pack several process into methods)

--- Imagic/app/src/main/java/com/imagic/imagic/Image.java
+++ Imagic/app/src/main/java/com/imagic/imagic/Image.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.util.Log;
 
 import org.json.JSONObject;
 
@@ -126,9 +125,9 @@ class Image implements JSONSerializable {
     }
 
     public void updateBitmap() {
-        int[] newRed = redHistogram.getNewEqualizedValue();
-        int[] newGreen = greenHistogram.getNewEqualizedValue();
-        int[] newBlue = blueHistogram.getNewEqualizedValue();
+        int[] newRed = redHistogram.getNewColorValueMap();
+        int[] newGreen = greenHistogram.getNewColorValueMap();
+        int[] newBlue = blueHistogram.getNewColorValueMap();
         int width = bitmap.getWidth();
         int height = bitmap.getHeight();
         Bitmap newBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
