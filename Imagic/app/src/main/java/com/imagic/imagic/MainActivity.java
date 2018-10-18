3f2e1c20f083aea10d795946e2926dfc97bd992a
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-10-18 19:04:13 +0700

    Tab experiment, change basic info

--- Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
@@ -1,99 +1,150 @@
 package com.imagic.imagic;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.provider.MediaStore;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Intent request codes
-    private enum RequestCode {
-        SELECT_IMAGE(0), CAPTURE_IMAGE(1);
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-        public int code;
-        RequestCode(int code) { this.code = code; }
-    }
-
-    // Selected or captured image URI
-    private Uri imageURI;
-
-    // Cached image data URI
-    private Uri cachedImageDataURI;
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cachedImageDataURI = Cache.NO_CACHE_URI;
+        Toolbar toolbar = (Toolbar) findViewById(R.id.titleBar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        Button selectImageButton = findViewById(R.id.selectImageButton);
-        Button captureImageButton = findViewById(R.id.captureImageButton);
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.viewContainer);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        selectImageButton.setOnClickListener(getSelectImageButtonOnClickListener());
-        captureImageButton.setOnClickListener(getCaptureImageButtonOnClickListener());
-    }
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
 
-    // Select image button on click listener
-    private View.OnClickListener getSelectImageButtonOnClickListener() {
-        return new View.OnClickListener() {
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType(Image.MIME_TYPE);
-                startActivityForResult(intent, RequestCode.SELECT_IMAGE.code);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        };
+        });
+
     }
 
-    // Capture image button on click listener
-    private View.OnClickListener getCaptureImageButtonOnClickListener() {
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
-                if(intent.resolveActivity(getPackageManager()) != null) {
-                    try {
-                        imageURI = Cache.create(MainActivity.this, new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()), "jpg", true);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageURI);
-                        startActivityForResult(intent, RequestCode.CAPTURE_IMAGE.code);
-                    }
-                    catch(Exception e) {
-                        Log.e("Imagic", "Exception", e);
-                    }
-                }
-            }
-        };
-    }
-
-    // Select or capture image result
+    /**
+     * Hides menu.
+     */
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == RESULT_OK) {
-            Cache.deleteOldCache(cachedImageDataURI);
-            if(requestCode == RequestCode.SELECT_IMAGE.code) imageURI = data.getData();
+    public boolean onCreateOptionsMenu(Menu menu) { return false; }
 
-            try {
-                cachedImageDataURI = Cache.create(this, "imagic", "cache", false);
-                Image image = new Image(this, imageURI);
-                Cache.write(cachedImageDataURI, JSONSerializer.serialize(image));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-                Intent intent = new Intent(this, MenuActivity.class);
-                intent.putExtra(Cache.INTENT_BUNDLE_NAME, cachedImageDataURI.toString());
-                startActivity(intent);
-            }
-            catch(Exception e) {
-                Log.e("Imagic", "Exception", e);
-            }
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
         }
     }
 }
