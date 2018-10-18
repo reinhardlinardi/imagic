7f2623269a8bd102f1254fa2367ba74402347046
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-10-18 21:42:16 +0700

    Change placeholder with individual fragments

--- Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
+++ Imagic/app/src/main/java/com/imagic/imagic/MainActivity.java
@@ -9,11 +9,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,50 +35,25 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {}
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup viewContainer, Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, viewContainer, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
+    // Adapter for menu
     public class MenuAdapter extends FragmentStatePagerAdapter {
 
+        // Constructor
         MenuAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
         }
 
+        // Get n-th fragment
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch(position) {
+                case 0 : return HistogramFragment.newInstance("a", "b");
+                case 1 : return ContrastEnhancementFragment.newInstance("a", "b");
+                case 2 : return EqualizerFragment.newInstance("a", "b");
+                case 3 : return OCRFragment.newInstance("a", "b");
+            }
+
+            return null;
         }
 
         // Return number of menu
