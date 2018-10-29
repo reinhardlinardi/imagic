5411c23f9c008c97cc25207474019037b6d56f65
Roland Hartanto <rolandhartanto@gmail.com>
2018-10-29 17:01:49 +0700

    add median operator

--- Imagic/app/src/main/java/com/imagic/imagic/EdgeDetector.java
+++ Imagic/app/src/main/java/com/imagic/imagic/EdgeDetector.java
@@ -1,15 +1,20 @@
 package com.imagic.imagic;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class EdgeDetector {
     int MEDIAN = 0;
     int DIFFERENCE = 1;
     int HOMOGENOUS_DIFFERENCE = 2;
     int[][] imageBitmap;
+    int[][] convolutedImageBitmap;
 
     EdgeDetector(int[][] imageBitmap) {
         int width = imageBitmap[0].length;
         int height = imageBitmap.length;
         this.imageBitmap = new int[height + 2][width + 2];
+        this.convolutedImageBitmap = new int[height][width];
 
         // pad image with 0 border
         for(int row = 0; row < height; row++) {
@@ -20,19 +25,43 @@ public class EdgeDetector {
     }
     // Smooth image
     void smoothenImage() {
-
+        convolute(MEDIAN);
     }
 
-    void sharpenImage() {
-
+    void sharpenImageEdge(int operatorCode) {
+        convolute(operatorCode);
     }
 
     void convolute(int operatorCode) {
-        
+        int width = imageBitmap[0].length;
+        int height = imageBitmap.length;
+        int[][] kernel = new int[3][3];
+
+        for(int row = 1; row < height - 1; row++) {
+            for(int col = 1; col < width - 1; col++) {
+                for(int i = 0; i < 3; i++) {
+                    for(int j = 0; j < 3; j++) {
+                        kernel[i][j] = imageBitmap[row - 1 + i][col - 1 + j];
+                    }
+                }
+
+                convolutedImageBitmap[row - 1][col - 1] = computeNewColorValue(kernel, operatorCode);
+            }
+        }
     }
 
-    int computeNewColorValue(int[][] inputMatrix, int operatorCode) {
+    int computeNewColorValue(int[][] kernel, int operatorCode) {
         int result = 0;
+        if (operatorCode == MEDIAN) {
+            ArrayList<Integer> valueList = new ArrayList<>();
+            for(int i = 0; i < 3; i++) {
+                for(int j = 0; j < 3; j++) {
+                    valueList.add(kernel[i][j]);
+                }
+            }
+            Collections.sort(valueList);
+            result = valueList.get(4);
+        }
 
         return result;
     }
