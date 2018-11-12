ed393e38be4d1c897bad0f05d45dcbddfec07691
FranciscoKen <franciscokenandi@gmail.com>
2018-11-12 21:14:34 +0700

    implemented simple temporary layout

---
+++ Imagic/app/src/main/java/com/imagic/imagic/FaceRecognitionFragment.java
@@ -0,0 +1,188 @@
+package com.imagic.imagic;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class FaceRecognitionFragment extends Fragment implements MainActivityListener {
+
+    /* Properties */
+
+    /* Communicator Variables */
+    private FragmentListener activity;
+
+    /* UI Components */
+    private LinearLayout container;
+    private ProgressBar progressBar;
+
+    private ImageView imageView;
+    private ImageView transformedImageView;
+
+    private TextView helpTextView;
+
+    Image image;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        activity = (FragmentListener) context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_face_recognition,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        if(isAttachedToMainActivity()){
+            container = view.findViewById(R.id.faceRecognitionContainer);
+
+            progressBar = view.findViewById(R.id.faceRecognitionProgressBar);
+            helpTextView = view.findViewById(R.id.faceRecognitionHelpTextView);
+
+            transformedImageView = view.findViewById(R.id.faceRecognitionTransformedImageView);
+            imageView = view.findViewById(R.id.faceRecognitionImageView);
+            imageView.setOnClickListener(getImageViewOnClickListener());
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        UI.clearMemory(getContext());
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        activity = null;
+    }
+
+    /* Implemented interface methods */
+
+    // Send given intent to select image
+    public void sendSelectImageIntent(Intent intent) { startActivityForResult(intent, IntentRequestCode.SELECT_IMAGE.code); }
+
+    // Send given intent to capture image
+    public void sendCaptureImageIntent(Intent intent) { startActivityForResult(intent, IntentRequestCode.CAPTURE_IMAGE.code); }
+
+    public void loadImageOnSelected(){
+        if(isAttachedToMainActivity()){
+            if(activity.isImageHasBitmap()){
+                /*WOY IMPLEMENT */
+                ImageLoadAsynctask imageLoadAsynctask = new ImageLoadAsynctask();
+                imageLoadAsynctask.execute(false);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(isAttachedToMainActivity()){
+            if(activity.onImageIntentResult(requestCode,resultCode,data) && activity.hasImage()){
+                /*WOY IMPLEMENT */
+                ImageLoadAsynctask imageLoadAsynctask = new ImageLoadAsynctask();
+                imageLoadAsynctask.execute(true);
+            }
+        }
+    }
+
+    /* Event Listeners */
+    private View.OnClickListener getImageViewOnClickListener(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(isAttachedToMainActivity()){
+                    ImageDialogFragment imageDialog = new ImageDialogFragment();
+                    imageDialog.show(getFragmentManager(),ImageDialogFragment.TAG);
+                }
+            }
+        };
+    }
+
+    /* Methods */
+    //Constructor
+    public FaceRecognitionFragment(){}
+
+    public static SpecialEffectsFragment newInstance(){return new SpecialEffectsFragment();}
+
+    // Check if fragment is attached to MainActivity or not
+    private boolean isAttachedToMainActivity() { return activity != null; }
+
+    // Count progress
+    private int countProgress(int numTaskDone, int totalNumTask) {
+        float taskDoneFraction = (float) numTaskDone / totalNumTask;
+        return (int)(taskDoneFraction * 100);
+    }
+
+    /* Async tasks */
+    //Image load Asynctask
+    private class ImageLoadAsynctask extends AsyncTask<Boolean, Integer, Boolean> {
+        @Override
+        protected Boolean doInBackground(Boolean... bools) {
+            boolean executedFromIntentResult = bools[0];
+            publishProgress(countProgress(1,2));
+            try {
+                if(isAttachedToMainActivity() && executedFromIntentResult) activity.loadImageBitmap(imageView.getWidth(),imageView.getHeight());
+                publishProgress(countProgress(2,2));
+            } catch (Exception e) {
+                Debug.ex(e);
+            }
+
+            return executedFromIntentResult;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            if(isAttachedToMainActivity()){
+                UI.setUnclickable(imageView);
+                if(UI.isVisible(helpTextView)) UI.hide(helpTextView);
+                progressBar.setProgress(0);
+                UI.show(progressBar);
+            }
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            if(isAttachedToMainActivity()){
+                progressBar.setProgress(progress[0]);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(isAttachedToMainActivity()){
+                image = new Image(getContext(),activity.getImage());
+
+                UI.setImageView(getContext(),imageView,image.bitmap);
+                UI.setImageView(getContext(),transformedImageView,image.bitmap);
+
+                UI.clearMemory(getContext());
+                UI.setInvisible(progressBar);
+
+                UI.setClickable(imageView);
+
+                if(!UI.isVisible(transformedImageView)) UI.show(transformedImageView);
+                if(!UI.isVisible(container)) UI.show(container);
+
+            }
+        }
+    }
+}
